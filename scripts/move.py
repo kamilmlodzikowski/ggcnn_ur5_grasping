f7674aba9e288550cc9b32335c8377093f26f08b
kamilmlodzikowski <k.mlodzikowski@gmail.com>
2020-06-08 11:52:56 +0200

    added tool moving by ggcnn

---
+++ scripts/move.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import rospy
+import tools
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float32MultiArray
+import time
+
+def move(pose_g):
+    global last_z
+
+    if not (pose_g.data[0] == 0 or pose_g.data[1] == 0 or pose_g.data[2] <= 0.15 or pose_g.data[2] > last_z):
+        tools.move2(pose_g, pub, 0)
+    # else:
+    #     time_now = time.time()
+    #     if ok == True:
+    #         passed = int(time_now - time_before)
+    #         print passed
+    #         if passed >= 5:
+    #             time.sleep(0.5)
+    #             manipulator.grip(0)
+    #             time.sleep(5)
+    #             teraz = tools.get_pose(manipulator)
+    #             teraz[0] -= 0.08
+    #             teraz[1] -= 0.10
+    #             teraz[2] -= 0.10
+    #             trajectory = list()
+    #             trajectory.append(teraz)
+    #             manipulator.move(trajectory, False, a=0.1, v=0.8)
+    #             time.sleep(4)
+    #             tools.goto(box, manipulator)
+    #             tools.goto(start, manipulator)
+    #             last_z = 1000
+    #             ok = False
+    #             tools.check_joints(manipulator)
+
+rospy.init_node('move')
+pub = rospy.Publisher('/controller_ur/move_to_pose', PoseStamped)
+
+last_z = 1000
+ok = False
+
+pose_goal = rospy.Subscriber('/ggcnn/out/command', Float32MultiArray, move, queue_size=1)
+
+while not rospy.is_shutdown():
+    rospy.spin()
+
+
---
+++ scripts/tools.py
@@ -0,0 +1,198 @@
+# coding=utf-8
+
+import numpy as np
+# import robot_controller
+# import time
+from scipy.spatial.transform import Rotation
+# from std_msgs.msg import Float32MultiArray
+# from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+
+def move2(pose_g, publisher, min_z, rot_z=0):
+    pose = PoseStamped()
+
+    pose.pose.position.x = -pose_g.data[0]
+    pose.pose.position.y = -pose_g.data[1]+0.06
+    pose.pose.position.z = pose_g.data[2]-0.13
+
+    c = np.cos(-pose_g.data[3])
+    s = np.sin(-pose_g.data[3])
+
+    orientation_ggcnn = [[c, -s, 0, 0],
+                        [s, c, 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]]
+
+    r = Rotation.from_dcm(orientation_ggcnn)
+
+    quat = r.as_quat()
+
+    pose.pose.orientation.x = quat[0]
+    pose.pose.orientation.y = quat[1]
+    pose.pose.orientation.z = quat[2]
+    pose.pose.orientation.w = quat[3]
+
+    publisher.pub(pose)
+
+    # new_matrix = np.matmul(newish_matrix, orientation_ggcnn)
+
+    # rot_x_limit = [[1, 0, 0, 0],
+    #          [0, 0.707, 0.707, 0],
+    #          [0, -0.707, 0.707, 0],
+    #          [0, 0, 0, 1]]
+    # inv_mat = np.linalg.inv(rot_x_limit)
+    # limit = np.matmul(inv_mat, new_matrix)
+    # if limit[2][3] > min_z:
+    #     limit [2][3] = min_z
+    #     new_matrix = np.matmul(rot_x_limit, limit)
+
+
+    # should_do = np.matmul(inv_mat, akt_matrix)
+    # if should_do[2][3] > min_z-0.15:
+    #     return -1
+    # mat_to_calc = [[new_matrix[0][0], new_matrix[0][1], new_matrix[0][2]],
+    #                [new_matrix[1][0], new_matrix[1][1], new_matrix[1][2]],
+    #                [new_matrix[2][0], new_matrix[2][1], new_matrix[2][2]]]
+    #
+    # fi = np.arccos((np.trace(mat_to_calc) - 1) / 2)
+    #
+    # ux = 1 / (2 * np.sin(fi)) * (mat_to_calc[2][1] - mat_to_calc[1][2])
+    # uy = 1 / (2 * np.sin(fi)) * (mat_to_calc[0][2] - mat_to_calc[2][0])
+    # uz = 1 / (2 * np.sin(fi)) * (mat_to_calc[1][0] - mat_to_calc[0][1])
+    #
+    # rx = ux * fi
+    # ry = uy * fi
+    # rz = uz * fi
+    #
+    # pose_goal = [new_matrix[0][3], new_matrix[1][3], new_matrix[2][3], rx, ry, rz]
+    # pose_goal = np.transpose(pose_goal)
+    # trajectory = list()
+    # trajectory.append(pose_goal)
+    # manipulator.move(trajectory, False, a=a, v=v)
+
+    # akt_to_round = manipulator.get_pose()
+    # pos_akt = np.round(akt_to_round, 3)
+    # pos_cel = np.round(pose_goal, 3)
+    # time_before = time.time()
+
+    # while pos_akt[0] != pos_cel[0] or pos_akt[1] != pos_cel[1] or pos_akt[2] != pos_cel[2] or pos_akt[3] != pos_cel[3] or pos_akt[4] != pos_cel[4] or pos_akt[5] != pos_cel[5]:
+    #     akt_to_round = manipulator.get_pose()
+    #     pos_akt = np.round(akt_to_round, 3)
+    #     time_now = time.time()
+    #     passed = int(time_now - time_before )
+    #     if passed > 0.2:
+    #         return -1
+
+#
+# def get_pose(manipulator):
+#     # manipulator = robot_controller.Ur3(ip, port_write, port_read)
+#     pose = manipulator.get_pose()
+#     return pose
+#
+# def get_z_orienation(manipulator):
+#
+#     pose = manipulator.get_pose()
+#     matrix = [[1, 0, 0],
+#               [0, 0.707, 0.707],
+#               [0, -0.707, 0.707]]
+#
+#     inv_matrix = np.linalg.inv(matrix)
+#
+#     rotation = [pose[3], pose[4], pose[5]]
+#     r = Rotation.from_rotvec(rotation)
+#     matrix_from_axis = r.as_dcm()
+#
+#     tmp_matrix = [[matrix_from_axis[0][0], matrix_from_axis[0][1], matrix_from_axis[0][2]],
+#                   [matrix_from_axis[1][0], matrix_from_axis[1][1], matrix_from_axis[1][2]],
+#                   [matrix_from_axis[2][0], matrix_from_axis[2][1], matrix_from_axis[2][2]]]
+#
+#     tmp_matrix = np.matmul(tmp_matrix, inv_matrix)
+#
+#     new_r = Rotation.from_dcm(tmp_matrix)
+#
+#     euler = new_r.as_euler("XYZ", degrees=False)
+#     z_orientation = euler[2]
+#     return z_orientation
+#
+#
+# def get_joints(manipulator):
+#     joints = manipulator.get_joints()
+#     return joints
+#
+#
+# def set_box(manipulator):
+#     print("Move arm to box position")
+#     raw_input("Press enter...")
+#     pose = manipulator.get_pose()
+#     return pose
+#
+#
+# def set_start(manipulator):
+#     print("Move arm to start position")
+#     raw_input("Press enter...")
+#     pose = manipulator.get_pose()
+#     manipulator.grip(150)
+#     return pose
+#
+#
+# def goto(pose, manipulator):
+#     trajectory = list()
+#     trajectory.append(pose)
+#     manipulator.move(trajectory, False, a=0.1, v=0.8)
+#
+#     akt_to_round = manipulator.get_pose()
+#     pos_akt = np.round(akt_to_round, 3)
+#     pos_cel = np.round(pose, 3)
+#     time_before = time.time()
+#     while pos_akt[0] != pos_cel[0] or pos_akt[1] != pos_cel[1] or pos_akt[2] != pos_cel[2] or pos_akt[3] != pos_cel[3] or pos_akt[4] != pos_cel[4] or pos_akt[5] != pos_cel[5]:
+#         akt_to_round = manipulator.get_pose()
+#         pos_akt = np.round(akt_to_round, 3)
+#         time_now = time.time()
+#         passed = int(time_now - time_before )
+#         if passed > 15:
+#             return -1
+#
+#     manipulator.grip(150)
+#     time.sleep(2)
+#     return 0
+#
+#
+# def set_table_z(manipulator):
+#     manipulator.grip(0)
+#     time.sleep(3)
+#     print("Touch the table with arm")
+#     raw_input("Press enter...")
+#     akt_pose = get_pose(manipulator)
+#     rot_x = [[1, 0, 0, 0],
+#              [0, 0.707, 0.707, 0],
+#              [0, -0.707, 0.707, 0],
+#              [0, 0, 0, 1]]
+#     inv_mat = np.linalg.inv(rot_x)
+#     akt_pose = [[1, 0, 0, akt_pose[0]],
+#                 [0, 1, 0, akt_pose[1]],
+#                 [0, 0, 1, akt_pose[2]],
+#                 [0, 0, 0, 1]]
+#     calc_pose = np.matmul(inv_mat, akt_pose)
+#     return calc_pose[2][3]
+#
+#
+# def check_joints(manipulator):
+#     joints = manipulator.get_joints()
+#     if joints[5] > np.pi or joints[5] < -np.pi:
+#         joints = np.delete(joints, 5)
+#         joints = np.append(joints, 0)
+#         trajectory = list()
+#         trajectory.append(joints)
+#         manipulator.move(trajectory, is_movej=True, is_pose=False, a=0.2, v=0.6)
+#         pos_akt = manipulator.get_joints()
+#         pos_akt = np.round(pos_akt, 2)
+#         joints = np.round(joints, 2)
+#         time_before = time.time()
+#         while pos_akt[5] != joints[5]:
+#             akt_to_round = manipulator.get_joints()
+#             pos_akt = np.round(akt_to_round, 2)
+#             time_now = time.time()
+#             passed = int(time_now - time_before)
+#             if passed > 10:
+#                 return -1
